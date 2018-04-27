0d4a36ff5571a4af23c452fec19fa49625453abb
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-04-27 23:53:38 +0530

    Added folder and cmake file

---
+++ Code/bin/external/glfw-3.1.2/examples/CMakeFiles/gears.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/external/glfw-3.1.2/examples/gears.c" "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/bin/external/glfw-3.1.2/examples/CMakeFiles/gears.dir/gears.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../bin/external/glfw-3.1.2/examples"
+  "../external/glfw-3.1.2/examples"
+  "../external/glfw-3.1.2/include"
+  "../external/glfw-3.1.2/deps"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/bin/external/glfw-3.1.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Code/build/CMakeFiles/Starlings.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/src/main.cpp" "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/build/CMakeFiles/Starlings.dir/src/main.cpp.o"
+  "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/src/shader.cpp" "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/build/CMakeFiles/Starlings.dir/src/shader.cpp.o"
+  "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/src/texture.cpp" "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/build/CMakeFiles/Starlings.dir/src/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../external/glfw-3.1.2/include"
+  "../external/glm-0.9.7.1"
+  "../external/glew-1.13.0/include"
+  "../."
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/bin/external/glfw-3.1.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/ankurshaswat/MyFiles/Repos/Starlings/Code/bin/external/CMakeFiles/GLEW_1130.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

8e87f098e5d746c620446f3988fc4269a632c1f7
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-04-28 19:23:25 +0530

    Incorprorated OBJ loader

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -22,6 +22,7 @@ using namespace glm;
 
 #include <shader.hpp>
 #include <texture.hpp>
+#include <objloader.hpp>
 #include <math.h>
 #include "glm/gtx/string_cast.hpp"
 // #include <common/controls.hpp>
@@ -29,11 +30,8 @@ using namespace glm;
 #define PI 3.14f
 
 
-GLuint programID;
-GLuint MatrixID;
-GLuint ViewMatrixID;
-GLuint ModelMatrixID;
-
+GLuint programID,MatrixID,ViewMatrixID,ModelMatrixID;
+std::vector<glm::vec3> vertices;
 glm::mat4 ViewMatrix,ProjectionMatrix,MVP,ModelMatrix,ScalingMatrix,TranslationMatrix,RotationMatrix;
 
 void renderBoid(vec3 pos);
@@ -121,7 +119,18 @@ int main( void )
 																0.0f,1.0f,0.0f,
 																0.0f,-1.0f,0.0f,
 																0.0f,0.0f,1.0f,
-																0.0f,0.0f,-1.0f,//aaxi
+																0.0f,0.0f,-1.0f,//axi
+
+																0.0f,1.0f,0.0f,//
+																-1.0f,-1.0f,0.0f,//axi
+															1.0f,-1.0f,0.0f,//axi
+
+															0.0f,0.0f,-1.0f,//
+															-1.0f,-1.0f,0.0f,//axi
+														1.0f,-1.0f,0.0f,//axi
+
+
+
 																-1.0f,-1.0f,-1.0f,
 																-1.0f,-1.0f, 1.0f,
 																-1.0f, 1.0f, 1.0f,
@@ -203,20 +212,191 @@ int main( void )
 																0.517f,  0.713f,  0.338f,
 																0.053f,  0.959f,  0.120f,
 																0.393f,  0.621f,  0.362f,
+
+																0.583f,  0.771f,  0.014f,
+																0.609f,  0.115f,  0.436f,
+																0.327f,  0.483f,  0.844f,
+																0.822f,  0.569f,  0.201f,
+																0.435f,  0.602f,  0.223f,
+																0.310f,  0.747f,  0.185f,
+																0.597f,  0.770f,  0.761f,
+																0.559f,  0.436f,  0.730f,
+																0.359f,  0.583f,  0.152f,
+																0.483f,  0.596f,  0.789f,
+																0.559f,  0.861f,  0.639f,
+																0.195f,  0.548f,  0.859f,
+																0.014f,  0.184f,  0.576f,
+																0.771f,  0.328f,  0.970f,
+																0.406f,  0.615f,  0.116f,
+																0.676f,  0.977f,  0.133f,
+																0.971f,  0.572f,  0.833f,
+																0.140f,  0.616f,  0.489f,
+																0.997f,  0.513f,  0.064f,
+																0.945f,  0.719f,  0.592f,
+																0.543f,  0.021f,  0.978f,
+																0.279f,  0.317f,  0.505f,
+																0.167f,  0.620f,  0.077f,
+																0.347f,  0.857f,  0.137f,
+																0.055f,  0.953f,  0.042f,
+																0.714f,  0.505f,  0.345f,
+																0.783f,  0.290f,  0.734f,
+																0.722f,  0.645f,  0.174f,
+																0.302f,  0.455f,  0.848f,
+																0.225f,  0.587f,  0.040f,
+																0.517f,  0.713f,  0.338f,
+																0.053f,  0.959f,  0.120f,
+																0.393f,  0.621f,  0.362f,
+
+																0.583f,  0.771f,  0.014f,
+																0.609f,  0.115f,  0.436f,
+																0.327f,  0.483f,  0.844f,
+																0.822f,  0.569f,  0.201f,
+																0.435f,  0.602f,  0.223f,
+																0.310f,  0.747f,  0.185f,
+																0.597f,  0.770f,  0.761f,
+																0.559f,  0.436f,  0.730f,
+																0.359f,  0.583f,  0.152f,
+																0.483f,  0.596f,  0.789f,
+																0.559f,  0.861f,  0.639f,
+																0.195f,  0.548f,  0.859f,
+																0.014f,  0.184f,  0.576f,
+																0.771f,  0.328f,  0.970f,
+																0.406f,  0.615f,  0.116f,
+																0.676f,  0.977f,  0.133f,
+																0.971f,  0.572f,  0.833f,
+																0.140f,  0.616f,  0.489f,
+																0.997f,  0.513f,  0.064f,
+																0.945f,  0.719f,  0.592f,
+																0.543f,  0.021f,  0.978f,
+																0.279f,  0.317f,  0.505f,
+																0.167f,  0.620f,  0.077f,
+																0.347f,  0.857f,  0.137f,
+																0.055f,  0.953f,  0.042f,
+																0.714f,  0.505f,  0.345f,
+																0.783f,  0.290f,  0.734f,
+																0.722f,  0.645f,  0.174f,
+																0.302f,  0.455f,  0.848f,
+																0.225f,  0.587f,  0.040f,
+																0.517f,  0.713f,  0.338f,
+																0.053f,  0.959f,  0.120f,
+																0.393f,  0.621f,  0.362f,
+
+																0.583f,  0.771f,  0.014f,
+																0.609f,  0.115f,  0.436f,
+																0.327f,  0.483f,  0.844f,
+																0.822f,  0.569f,  0.201f,
+																0.435f,  0.602f,  0.223f,
+																0.310f,  0.747f,  0.185f,
+																0.597f,  0.770f,  0.761f,
+																0.559f,  0.436f,  0.730f,
+																0.359f,  0.583f,  0.152f,
+																0.483f,  0.596f,  0.789f,
+																0.559f,  0.861f,  0.639f,
+																0.195f,  0.548f,  0.859f,
+																0.014f,  0.184f,  0.576f,
+																0.771f,  0.328f,  0.970f,
+																0.406f,  0.615f,  0.116f,
+																0.676f,  0.977f,  0.133f,
+																0.971f,  0.572f,  0.833f,
+																0.140f,  0.616f,  0.489f,
+																0.997f,  0.513f,  0.064f,
+																0.945f,  0.719f,  0.592f,
+																0.543f,  0.021f,  0.978f,
+																0.279f,  0.317f,  0.505f,
+																0.167f,  0.620f,  0.077f,
+																0.347f,  0.857f,  0.137f,
+																0.055f,  0.953f,  0.042f,
+																0.714f,  0.505f,  0.345f,
+																0.783f,  0.290f,  0.734f,
+																0.722f,  0.645f,  0.174f,
+																0.302f,  0.455f,  0.848f,
+																0.225f,  0.587f,  0.040f,
+																0.517f,  0.713f,  0.338f,
+																0.053f,  0.959f,  0.120f,
+																0.393f,  0.621f,  0.362f,
+
+																0.583f,  0.771f,  0.014f,
+																0.609f,  0.115f,  0.436f,
+																0.327f,  0.483f,  0.844f,
+																0.822f,  0.569f,  0.201f,
+																0.435f,  0.602f,  0.223f,
+																0.310f,  0.747f,  0.185f,
+																0.597f,  0.770f,  0.761f,
+																0.559f,  0.436f,  0.730f,
+																0.359f,  0.583f,  0.152f,
+																0.483f,  0.596f,  0.789f,
+																0.559f,  0.861f,  0.639f,
+																0.195f,  0.548f,  0.859f,
+																0.014f,  0.184f,  0.576f,
+																0.771f,  0.328f,  0.970f,
+																0.406f,  0.615f,  0.116f,
+																0.676f,  0.977f,  0.133f,
+																0.971f,  0.572f,  0.833f,
+																0.140f,  0.616f,  0.489f,
+																0.997f,  0.513f,  0.064f,
+																0.945f,  0.719f,  0.592f,
+																0.543f,  0.021f,  0.978f,
+																0.279f,  0.317f,  0.505f,
+																0.167f,  0.620f,  0.077f,
+																0.347f,  0.857f,  0.137f,
+																0.055f,  0.953f,  0.042f,
+																0.714f,  0.505f,  0.345f,
+																0.783f,  0.290f,  0.734f,
+																0.722f,  0.645f,  0.174f,
+																0.302f,  0.455f,  0.848f,
+																0.225f,  0.587f,  0.040f,
+																0.517f,  0.713f,  0.338f,
+																0.053f,  0.959f,  0.120f,
+																0.393f,  0.621f,  0.362f,
 																0.673f,  0.211f,  0.457f,
 																0.820f,  0.883f,  0.371f,
 																0.982f,  0.099f,  0.879f//cube
 								};
 
+								// Read our .obj file
+								// std::vector<glm::vec3> vertices;
+								vertices.push_back(vec3(1.0f,0.0f,0.0f));
+								vertices.push_back(vec3(-1.0f,0.0f,0.0f));
+								vertices.push_back(vec3(0.0f,1.0f,0.0f));
+								vertices.push_back(vec3(0.0f,-1.0f,0.0f));
+								vertices.push_back(vec3(0.0f,0.0f,1.0f));
+								vertices.push_back(vec3(0.0f,0.0f,-1.0f));
+
+								vertices.push_back(vec3(0.0f,1.0f,0.0f));
+								vertices.push_back(vec3(-1.0f,-1.0f,0.0f));
+								vertices.push_back(vec3(1.0f,-1.0f,0.0f));
+
+								vertices.push_back(vec3(0.0f,1.0f,0.0f));
+								vertices.push_back(vec3(0.0f,-1.0f,-1.0f));
+								vertices.push_back(vec3(0.0f,-1.0f,1.0f));
+
+								std::vector<glm::vec2> uvs;
+								std::vector<glm::vec3> normals; // Won't be used at the moment.
+								bool res = loadOBJ("cube.obj", vertices, uvs, normals);
+
+								// Load it into a VBO
+
 								GLuint vertexbuffer;
 								glGenBuffers(1, &vertexbuffer);
 								glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-								glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+								glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+
+								GLuint uvbuffer;
+								glGenBuffers(1, &uvbuffer);
+								glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+								glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
+
+
+								// GLuint vertexbuffer;
+								// glGenBuffers(1, &vertexbuffer);
+								// glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+								// glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 								GLuint colorbuffer;
 								glGenBuffers(1, &colorbuffer);
 								glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 								glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
 								vec3 gPosition1(0.0f, 0.0f, 0.0f);
 								vec3 gOrientation1;
 								double xpos, ypos,prevxpos,prevypos;
@@ -292,7 +472,7 @@ int main( void )
 
 																// cout<<"CamX :- "<<camX<<endl;
 																// cout<<" CamY :- "<<camY<<endl;
-																cout<< "Position = "<<glm::to_string(rotated)<<endl;
+																// cout<< "Position = "<<glm::to_string(rotated)<<endl;
 
 																i++;
 																double currentTime = glfwGetTime();
@@ -370,6 +550,18 @@ int main( void )
 																								(void*)0     // array buffer offset
 																								);
 
+																// 3rd attribute buffer : UVs
+																glEnableVertexAttribArray(2);
+																glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+																glVertexAttribPointer(
+																								2,                   // attribute. No particular reason for 1, but must match the layout in the shader.
+																								3,                   // size
+																								GL_FLOAT,            // type
+																								GL_FALSE,            // normalized?
+																								0,                   // stride
+																								(void*)0             // array buffer offset
+																								);
+
 																// Draw the triangle !
 																glDrawArrays(GL_LINES, 0, 3*2); // 12*3 indices starting at 0 -> 12 triangles
 
@@ -414,6 +606,7 @@ int main( void )
 
 																glDisableVertexAttribArray(0);
 																glDisableVertexAttribArray(1);
+																glDisableVertexAttribArray(2);
 
 																// Swap buffers
 																glfwSwapBuffers(window);
@@ -492,7 +685,7 @@ void renderFlock(std::vector<vec3> v){
 
 void renderBoid(vec3 pos){
 								TranslationMatrix = translate(mat4(), pos); // A bit to the right
-								ScalingMatrix = scale(mat4(), vec3(0.01f, 0.01f,0.01f));
+								ScalingMatrix = scale(mat4(), vec3(0.1f));
 								ModelMatrix = TranslationMatrix * RotationMatrix * ScalingMatrix;
 
 								MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
@@ -500,6 +693,11 @@ void renderBoid(vec3 pos){
 								glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
 								glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
 
-								glDrawArrays(GL_TRIANGLES, 6, 12*3); // 12*3 indices starting at 0 -> 12 triangles
+
+								// Draw the triangle !
+								// glDrawArrays(GL_TRIANGLES, 6, vertices.size() );
+								glDrawArrays(GL_TRIANGLES, 6, 3*2 );
+
+								// glDrawArrays(GL_TRIANGLES, 6, 12*3); // 12*3 indices starting at 0 -> 12 triangles
 
 }
